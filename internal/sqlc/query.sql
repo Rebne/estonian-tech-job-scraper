f66fcc78099cab5f3a70a80140395eca465c3386
Rebne <renehgren@gmail.com>
2026-03-14 00:06:23 +0200

    Add other queries from v1

--- internal/sqlc/query.sql
+++ internal/sqlc/query.sql
@@ -1,2 +1,14 @@
--- name: ListAllJobs :many
+-- name: GetAllJobs :many
 SELECT * FROM jobs;
+
+-- name: GetAllTitles :many
+SELECT title FROM jobs;
+
+-- name: InsertJob :exec
+INSERT INTO jobs (job_hash, page, title) VALUES($1, $2, $3);
+
+-- name: DeleteJob :exec
+DELETE FROM jobs WHERE job_hash = $1 AND page = $2;
+
+-- name: DeleteJobByID :exec
+DELETE FROM jobs WHERE id = $1;
