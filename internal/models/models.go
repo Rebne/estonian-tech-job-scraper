0a067223d309bd98e6e85765227560a92a0e2b1b
Rebne <renehgren@gmail.com>
2026-03-14 00:29:30 +0200

    Add job model and models package

---
+++ internal/models/models.go
@@ -0,0 +1,14 @@
+package models
+
+type job struct {
+	hash           []byte
+	page           string
+	title          string
+	company        string
+	location       string
+	description    string
+	employmentType string
+	category       string
+	url            string
+}
+
