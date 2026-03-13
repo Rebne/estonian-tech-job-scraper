66f624004c6ec2e86631b53dff93d7577f670b4d
Rebne <renehgren@gmail.com>
2026-03-14 00:29:57 +0200

    Create job builder

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "crypto/sha256"
+
 type job struct {
 	hash           []byte
 	page           string
@@ -12,3 +14,56 @@ type job struct {
 	url            string
 }
 
+type JobBuilder struct {
+	job *job
+}
+
+func (jb *JobBuilder) Build() *job {
+	return jb.job
+}
+
+func (jb *JobBuilder) WithURL(url string) *JobBuilder {
+	jb.job.url = url
+	return jb
+}
+
+func (jb *JobBuilder) WithCategory(category string) *JobBuilder {
+	jb.job.category = category
+	return jb
+}
+
+func (jb *JobBuilder) WithEmploymentType(employmentType string) *JobBuilder {
+	jb.job.employmentType = employmentType
+	return jb
+}
+
+func (jb *JobBuilder) WithDescription(description string) *JobBuilder {
+	jb.job.description = description
+	return jb
+}
+
+func (jb *JobBuilder) WithLocation(location string) *JobBuilder {
+	jb.job.location = location
+	return jb
+}
+
+func (jb *JobBuilder) WithCompany(company string) *JobBuilder {
+	jb.job.company = company
+	return jb
+}
+
+func (jb *JobBuilder) WithTile(title string) *JobBuilder {
+	jb.job.title = title
+	return jb
+}
+
+func (jb *JobBuilder) WithPage(page string) *JobBuilder {
+	jb.job.page = page
+	return jb
+}
+
+func (jb *JobBuilder) WithUniqueIdentifier(identifier string) *JobBuilder {
+	sum := sha256.Sum256([]byte(identifier))
+	jb.job.hash = sum[:]
+	return jb
+}
