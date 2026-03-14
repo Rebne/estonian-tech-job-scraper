476d65c3d8d738c389c984d002694210787df54d
Rebne <renehgren@gmail.com>
2026-03-14 13:35:28 +0200

    Export include and exclude keywords from v1

--- internal/services/jobfilter/jobfilter.go
+++ internal/services/jobfilter/jobfilter.go
@@ -1,2 +1,34 @@
 package jobfilter
 
+var excludeKeywords = []string{
+	"staff",
+	"lektor",
+	"ekspert",
+	"owner",
+	"omanik",
+	"ohvitser",
+	"consultant",
+	"konsultant",
+	"arhitekt",
+	"architect",
+	"vanem",
+	"senior",
+	"lead",
+	"juht",
+	"manager",
+}
+
+var includeKeywords = []string{
+	"arendaja",
+	"developer",
+	"full-stack",
+	"full stack",
+	"engineer",
+	"junior",
+	"algaja",
+	"intern",
+	"praktikant",
+	"tester",
+	"testija",
+	"ui/ux",
+}
