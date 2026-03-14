9f6c1743a2ce80fc484e3a8f6af48298ff59f37b
Rebne <renehgren@gmail.com>
2026-03-15 00:12:18 +0200

    Implement main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"github.com/Rebne/scrapy_project_v2/internal/app"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	config, err := app.BuildConfig()
+	if err != nil {
+		log.Fatal("building config failed: ", err)
+	}
+	pool, err := pgxpool.New(context.Background(), config.DatabaseURL)
+	if err != nil {
+		log.Fatal("cannot connect to database: ", err)
+	}
+
+	if err := pool.Ping(context.Background()); err != nil {
+		log.Fatal("database unreachable:", err)
+	}
+
+	log.Println("PostgreSQL connected")
+
+	app.NewRunner(config, pool).Run(context.Background())
 }
