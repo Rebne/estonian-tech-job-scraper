0282d177b2aea87b8b932a4a4eee4b186a34c4ae
Rebne <renehgren@gmail.com>
2026-03-21 21:39:43 +0200

    Bypass recaptcha for chromedp

--- internal/scrape/fetcher/chrome.go
+++ internal/scrape/fetcher/chrome.go
@@ -7,21 +7,32 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-
 func FetchHTMLViaChrome(ctx context.Context, url string) (string, error) {
-	ctx, cancel := chromedp.NewContext(ctx)
-		defer cancel()
+	allocCtx, allocCancel := chromedp.NewExecAllocator(ctx,
+		chromedp.Flag("headless", true),
+		chromedp.Flag("no-sandbox", true),
+		chromedp.Flag("disable-setuid-sandbox", true),
+		chromedp.Flag("disable-dev-shm-usage", true),
+		chromedp.Flag("disable-blink-features", "AutomationControlled"),
+		chromedp.Flag("window-size", "1366,768"),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"),
+	)
+	defer allocCancel()
 
-		var html string
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
 
-		err := chromedp.Run(ctx,
-			chromedp.Navigate(url),
-			chromedp.WaitVisible("body", chromedp.ByQuery),
-			chromedp.OuterHTML("html", &html),
-		)
-		if err != nil {
-			return "", fmt.Errorf("chromedp run ended with and error: %w", err)
-		}
+	var html string
 
-		return html, nil
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible("body", chromedp.ByQuery),
+		chromedp.OuterHTML("html", &html),
+	)
+	if err != nil {
+		return "", fmt.Errorf("chromedp run ended with an error: %w", err)
+	}
+
+	return html, nil
 }
