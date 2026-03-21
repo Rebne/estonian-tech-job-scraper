93ff0e07f435cf5c2423cd2aca38ec2354adb495
Rebne <renehgren@gmail.com>
2026-03-21 15:35:26 +0200

    Add stdOutNotifier
    Add a notifier that can be used for just logging to stdout

--- pkg/notifier/notifier.go
+++ pkg/notifier/notifier.go
@@ -1,5 +1,20 @@
 package notifier
 
+import "fmt"
+
 type Notifier interface {
 	Notify(messages []string) error
+}
+
+type stdOutNotifier struct {}
+
+func NewStdOutNotifier() *stdOutNotifier {
+	return &stdOutNotifier{}
+}
+
+func (son stdOutNotifier) Notify(messages []string) error {
+	for _, message := range messages {
+		fmt.Println(message)
+	}
+	return nil
 }
