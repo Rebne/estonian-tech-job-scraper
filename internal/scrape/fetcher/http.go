175147fe6f1c9318db21d8a55e53dbc9539b4424
Rebne <renehgren@gmail.com>
2026-03-21 22:33:13 +0200

    Adjust http fetch to new interface

--- internal/scrape/fetcher/http.go
+++ internal/scrape/fetcher/http.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+type HTTPFetcher struct{}
+
+func NewHTTPFetcher() *HTTPFetcher {
+	return &HTTPFetcher{}
+}
+
+func (hf *HTTPFetcher) Fetch(ctx context.Context, url string) (string, error) {
+	return FetchHTMLViaHTTP(ctx, url)
+}
+
+func (hf *HTTPFetcher) Close() error {
+	return nil
+}
+
 func FetchHTMLViaHTTP(ctx context.Context, url string) (string, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
