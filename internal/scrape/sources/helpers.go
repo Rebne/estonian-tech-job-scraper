066d3c0a5e2d2a742791c9b8dde89de535d7aef0
Rebne <renehgren@gmail.com>
2026-03-28 13:37:27 +0200

    Add ErrNoJobsFound to sources helpers

--- internal/scrape/sources/helpers.go
+++ internal/scrape/sources/helpers.go
@@ -3,7 +3,10 @@ package sources
 import (
 	"github.com/Rebne/scrapy_project_v2/internal/domain"
 	"github.com/Rebne/scrapy_project_v2/internal/services/jobfilter"
+	"errors"
 )
+
+var ErrNoJobsFound = errors.New("no jobs found")
 
 func filterJobs(jobs []domain.Job, filterChain jobfilter.JobFilterChain) []domain.Job {
 	var result []domain.Job
