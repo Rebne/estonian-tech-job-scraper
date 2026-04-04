7966486880e9e63474ff93c6a9c37c03c001aede
Rebne <renehgren@gmail.com>
2026-04-04 11:25:59 +0300

    Add proxy support to http fetcher

--- internal/scrape/fetcher/http.go
+++ internal/scrape/fetcher/http.go
@@ -2,25 +2,46 @@ package fetcher
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 )
 
-type HTTPFetcher struct{}
+type HTTPFetcher struct {
+	client *http.Client
+}
 
-func NewHTTPFetcher() *HTTPFetcher {
-	return &HTTPFetcher{}
+func NewHTTPFetcher(proxyURL string) (*HTTPFetcher, error) {
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.Proxy = nil
+
+	if proxyURL != "" {
+		parsedProxyURL, err := url.Parse(proxyURL)
+		if err != nil {
+			return nil, err
+		}
+		if parsedProxyURL.Scheme == "" || parsedProxyURL.Host == "" {
+			return nil, fmt.Errorf("missing scheme or host in proxyUrl")
+		}
+
+		transport.Proxy = http.ProxyURL(parsedProxyURL)
+	}
+
+	return &HTTPFetcher{
+		client: &http.Client{Transport: transport},
+	}, nil
 }
 
 func (hf *HTTPFetcher) Fetch(ctx context.Context, url string) (string, error) {
-	return FetchHTMLViaHTTP(ctx, url)
+	return hf.fetchHTMLViaHTTP(ctx, url)
 }
 
 func (hf *HTTPFetcher) Close() error {
 	return nil
 }
 
-func FetchHTMLViaHTTP(ctx context.Context, url string) (string, error) {
+func (hf *HTTPFetcher) fetchHTMLViaHTTP(ctx context.Context, url string) (string, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return "", err
@@ -29,7 +50,7 @@ func FetchHTMLViaHTTP(ctx context.Context, url string) (string, error) {
 	req.Header.Set("User-Agent",
 		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36")
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := hf.client.Do(req)
 	if err != nil {
 		return "", err
 	}
