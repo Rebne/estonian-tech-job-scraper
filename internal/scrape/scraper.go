e433f0210e28f28a7dede837f388d780306af90f
Rebne <renehgren@gmail.com>
2026-06-28 01:32:50 +0300

    Add ScrapeResult type

--- internal/scrape/scraper.go
+++ internal/scrape/scraper.go
@@ -9,4 +9,15 @@ import (
 type Scraper interface {
 	Name() string
 	GetJobs(context.Context) ([]domain.Job, error)
+type ScrapeStatus string
+
+const (
+	ScrapeStatusSuccess ScrapeStatus = "Success"
+	ScrapeStatusFailed  ScrapeStatus = "Failed"
+)
+
+type ScrapeResult struct {
+	Source string
+	Jobs   []domain.Job
+	Status ScrapeStatus
 }
